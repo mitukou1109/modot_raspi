812feb00fd23a6c8367a89b0dcec161b23cec578
mitukou1109 <mitukou1109@gmail.com>
2024-05-11 22:54:28 +0900

    run face recognition on PC

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -1,17 +1,8 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    publish_result_image = LaunchConfiguration("publish_result_image")
-
-    publish_result_image_argument = DeclareLaunchArgument(
-        "publish_result_image", default_value="true"
-    )
-
     v4l2_camera_node = Node(
         package="v4l2_camera",
         executable="v4l2_camera_node",
@@ -31,32 +22,6 @@ def generate_launch_description():
         }.items(),
     )
 
-    face_identifier_node = Node(
-        package="face_recognition_ros",
-        executable="face_identifier",
-        name="face_identifier",
-        output="screen",
-        remappings=[("image_raw", "/camera/image_raw")],
-        parameters=[
-            {
-                "publish_result_image": publish_result_image,
-                "resize_height": 160,
-            }
-        ],
-    )
-
-    result_image_republisher_node = Node(
-        package="image_transport",
-        executable="republish",
-        name="result_image_republisher",
-        arguments=["raw"],
-        remappings={
-            "in": "/face_identifier/result_image",
-            "out": "/face_identifier/result_image/compressed",
-        }.items(),
-        condition=IfCondition(publish_result_image),
-    )
-
     tactile_notifier_node = Node(
         package="modot_raspi",
         executable="tactile_notifier_node",
@@ -66,11 +31,8 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            publish_result_image_argument,
             v4l2_camera_node,
             image_raw_republisher_node,
-            face_identifier_node,
-            result_image_republisher_node,
             tactile_notifier_node,
         ]
     )
